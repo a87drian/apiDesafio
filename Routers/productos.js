cc0950e505e093393a3919fd4afb94f7b3a6f696
Adrian Chaves Ghilardi <adrian.chaves@gmail.com>
2021-10-11 23:34:04 -0300

    desafio ejs listo

--- Routers/productos.js
+++ Routers/productos.js
@@ -6,17 +6,6 @@ const containerProducts = new container('./Products/productos.json');
 
 const productosRouter = express.Router();
 
-productosRouter.get('/', async (req, res)=>{
-    const listProducts = await containerProducts.getAll();
-    //console.log(listProducts)
-    res.send(listProducts);
-})
-productosRouter.get('/:id', async (req, res) => {
-    console.log('linea 15')
-    const product = await containerProducts.getById(req.params.id);
-    res.send(product);
-})
-
 
 productosRouter.post('/', async (req, res)=>{
     const newProduct = req.body;
@@ -53,5 +42,32 @@ productosRouter.delete(':/id', async (req, res) => {
         message: "borrado ok"
     })
 
+})
+
+productosRouter.get('/list-productos', async (req, res) => {
+    const listProducts = await containerProducts.getAll();
+    res.render('pages/index', {
+        listProducts
+    });
+});
+productosRouter.get('/product', (req, res) => {
+    res.render('pages/product')
+})
+
+productosRouter.post('/form', async (req, res) => {
+    const newProduct = req.body;
+    console.log('body:' + newProduct)
+    const id = await containerProducts.save(newProduct);
+    res.redirect(`http://localhost:8080/api/productos/list-productos`)
+});
+productosRouter.get('/', async (req, res) => {
+    const listProducts = await containerProducts.getAll();
+    //console.log(listProducts)
+    res.send(listProducts);
+})
+productosRouter.get('/:id', async (req, res) => {
+    console.log('linea 15')
+    const product = await containerProducts.getById(req.params.id);
+    res.send(product);
 })
 module.exports = productosRouter;
--- server.js
+++ server.js
@@ -6,24 +6,10 @@ const server = express();
 server.set('view engine','ejs')
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
-//server.use('/static', express.static('public'))
+server.use('/static', express.static('public'))
 
 server.get('/', (req, res) => {
     res.send({message: new Date().toLocaleDateString()})
-})
-server.get('/list-productos', async (req, res) => {
-    //const listProducts = await containerProducts.getAll();
-    const listProducts = [{
-                "title": "Escuadra",
-                "price": 123.45,
-                "id": 1
-            },
-            {
-                "title": "Calculadora",
-                "price": 234.56,
-                "id": 2
-            }]
-    res.render('pages/index', {listProducts});
 })
 
 const PORT = 8080;
