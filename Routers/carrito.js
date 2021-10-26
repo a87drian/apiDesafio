0db1ac301b5f2acccd957a54c4f6ff919c29b7c3
Adrian Chaves Ghilardi <adrian.chaves@gmail.com>
2021-10-26 15:30:29 -0300

    primera entrega final

---
+++ Routers/carrito.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+
+const carritoRouter = express.Router();
+
+
+module.exports = carritoRouter;
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const productosRouter = require('./Routers/productos');
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
 
 const server = express();
+const httpServer = new HttpServer(server)
+const io = new IOServer(httpServer)
+
+
 
 server.set('view engine','ejs')
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
-server.use('/static', express.static('public'))
+// server.use('/static', express.static('public'))
+server.use(express.static('public'))
 
 server.get('/', (req, res) => {
     res.send({message: new Date().toLocaleDateString()})
@@ -15,8 +23,39 @@ server.get('/', (req, res) => {
 const PORT = 8080;
 
 server.use('/api/productos', productosRouter);
+server.use('/api/carrito', carritoRouter);
 
 
-server.listen(PORT, ()=> {
-    console.log(`server on ${PORT}`)
+
+
+// server.listen(PORT, ()=> {
+//     console.log(`server on ${PORT}`)
+// })
+
+httpServer.listen(8080, function () {
+    console.log('Servidor corriendo en http://localhost:8080');
+})
+
+const messages = [
+    {
+        author: "Juan",
+        text: "¡Hola! ¿Que tal?"
+    }, {
+        author: "Pedro",
+        text: "¡Muy bien! ¿Y vos?"
+    }, {
+        author: "Ana",
+        text: "¡Genial!"
+    }
+
+]
+
+io.on('connection', (socket) => { 
+    console.log('Usuario conectado')
+    socket.emit('messages:', messages)
+     socket.on('new-message', data => {
+         messages.push(data);
+         io.sockets.emit('messages', messages);
+     });
+
 })
