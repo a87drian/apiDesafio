212d98eaa838320af012513b724b5043c259915e
Adrian Chaves Ghilardi <adrian.chaves@gmail.com>
2021-10-30 13:33:48 -0300

    funcionando sin ninguna validacion

--- Routers/carritos.js
+++ Routers/carritos.js
@@ -6,23 +6,35 @@ const containerCarrito = new container('./Products/carrito.json');
 
 let Admin = true;
 // console.log('carrito.js '+Admin);
-carritoRouter.get('/', async (req, res) => {
-    const ListCarrito = await containerCarrito.getAll();
-    res.json(ListCarrito)
+
+carritoRouter.get('/:id/productos', async (req, res) => {
+    const idCarrito = req.params.id;
+    const Carrito = await containerCarrito.getById(idCarrito);
+    res.json(Carrito.productos)
 });
-carritoRouter.post('/',async (req, res)=> {
+carritoRouter.post('/:id/productos', async (req, res) => {
+    const idCarrito = req.params.id;
+    const carrito = await containerCarrito.getById(idCarrito);
+    const producto = req.body;
+    console.log('producto: ', req.body)
     
+    const carritoUpdate = await containerCarrito.update(idCarrito, producto);
+    res.json(carritoUpdate)
+
+
+})
+
+carritoRouter.post('/',async (req, res)=> {
+    console.log('post /')
     const carrito = new Object();
     carrito.create_at = new Date();
-    carrito.producto = [];
+    carrito.productos = [];
     console.log(carrito)
     idCarrito = await containerCarrito.save(carrito);
     res.json(idCarrito)
 } );
 
-carritoRouter.post('/:id/productos', (req, res) => {
 
-})
 carritoRouter.delete('/:id', async (req, res) => {
     const idCarrito = req.params.id
     const carritoDelete = await containerCarrito.deleteById(idCarrito);
@@ -37,7 +49,16 @@ carritoRouter.delete('/:id', async (req, res) => {
     }
 })
 
-carritoRouter.delete('/:id/productos/:id_prod', (req,res)=>{
-
+carritoRouter.delete('/:id/productos/:id_prod', async (req,res)=>{
+    console.log('/:id/productos/:id_prod')
+    const idCarrito = req.params.id
+    let carrito = await containerCarrito.getById(idCarrito);
+    // console.log('carrito', carrito)
+    const idProducto = req.params.id_prod;
+    
+    const removeID = carrito.productos.findIndex((pr) => pr.id === parseInt(idProducto));
+    carrito.productos.splice(removeID, 1);
+    const actualizado = await containerCarrito.update(idCarrito, carrito);
+    res.json('eliminado')
 }) 
 module.exports = carritoRouter;
--- contenedor.js
+++ contenedor.js
@@ -19,16 +19,10 @@ class Contenedor {
       } else {
         console.log("else");
         products = JSON.parse(content);
-        product.id = Math.max.apply(
-          Math,
-          products.map((o) => {
-            return o.id + 1;
-          })
-        );
+        //product.id = products[products.length - 1].id + 1;
+        let idmax = Math.max.apply(Math, products.map(o => o.id)) + 1;
+        product.id =idmax;
       }
-
-      console.log("guardar array");
-      console.log(products.length)
 
       fs.promises.writeFile(
         `./${this.filename}`,
@@ -42,19 +36,20 @@ class Contenedor {
     }
   }
   async getById(id) {
+    console.log('getById')
     // Recibe un id y devuelve el objeto con ese id, o null si no está.
-
+    
     try {
       const file = await fs.promises.readFile(`./${this.filename}`, "utf-8");
       //console.log(file)
       const products = JSON.parse(file);
-
+      console.log('getById id', id)
       const found = products.find((element) => {
         if (element.id === parseInt(id)) {
           return element;
         }
       });
-      console.log(found);
+      console.log('found', found);
       return found;
     } catch (error) {
       console.log("Error:" + error);
@@ -63,18 +58,35 @@ class Contenedor {
   async getAll() {
     // Devuelve un array con los objetos presentes en el archivo.
     const file = await fs.promises.readFile(`./${this.filename}`, "utf-8");
-    if (file) {
+    // console.log('file', file)
+    if (!file) {
       return "vacio";
     }
     const products = JSON.parse(file);
+    // console.log('getall', products)
     return products;
   }
   async deleteById(id) {
     //  Elimina del archivo el objeto con el id buscado.
     const file = await fs.promises.readFile(`./${this.filename}`, "utf-8");
     const products = JSON.parse(file);
-    const removeID = products.findIndex((pr) => pr.id === id);
-    const removed = products.splice(removeID, 1);
+    let removed
+    if (products === ''){}
+    else{
+      const removeID =  products.findIndex((pr) => pr.id === parseInt(id));
+      // console.log(products.findIndex((pr) => pr.id === parseInt(id)))
+      // console.log('removeID ' + removeID)
+      // console.log(products.splice(removeID, 1))
+      removed = products.splice(removeID, 1);
+      if (removeID !== -1) {
+        //const listActualizada = products.splice(removeID, 1);
+        //console.log('listActualizada' + listActualizada...)
+        fs.promises.writeFile(
+          `./${this.filename}`,
+          JSON.stringify(products, null, 2)
+        );
+      }
+     }
     return removed;
    // console.log("deleteById", products);
   }
@@ -92,9 +104,8 @@ class Contenedor {
   }
   async update(id, productUpdate) {
     const list = await this.getAll();
-    const productSaved = list.find((item) => {
-      item.id === parseInt(id);
-    });
+    const productSaved = list.find((item) => item.id === parseInt(id));
+    
     const indexProductSaved = list.findIndex(
       (item) => item.id === parseInt(id)
     );
@@ -114,6 +125,16 @@ class Contenedor {
       JSON.stringify(list, null, 2)
     );
     return productUpdated;
+  }
+  async updateCarrito(id, productUpdate) {
+    console.log('updateCarrito')
+    const list = await this.getAll();
+    const carritoSaved = list.find((item) => item.id === parseInt(id))
+    const indexCarritoSaved = list.findIndex((item) => item.id === parseInt(id));
+
+    list[indexCarritoSaved] = carritoSaved
+    console.log('list', list)
+    
   }
 }
 module.exports = Contenedor;
